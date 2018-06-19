e9cfebd2d6fb1e32def38d3df54b6110418f35af
Colby Sieber <colbys@microsoft.com>
2018-06-19 12:23:25 -0700

    Added support for checking multiple packages

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,23 @@
 #!/usr/bin/env node
 const { hasTypes, result } = require('../src/index');
 
-const [,, ...packages] = process.argv;
+const [,, ...args] = process.argv;
 
-if (packages.length === 0) {
-	console.log('Please provide at least one package name.');
+checkPackages(args);
+
+async function checkPackages(packages) {
+	if (packages.length === 0) {
+		console.log('Please provide at least one package name.');
+		return;
+	}
+
+	for (const package of packages) {
+		await checkPackage(package);
+	}
 }
 
 async function checkPackage(package) {
-	console.log(`Checking ${package}...`);
+	console.log(`Checking package "${package}"...`);
 
 	const value = await hasTypes(package);
 
@@ -20,10 +29,12 @@ async function checkPackage(package) {
 			console.log(`Yes! TypeScript typings are bundled with the package.`);
 			break;
 		case result.foundSeparate:
-			console.log(`Yes! Typings can be found in the @types/${package} package.`);
+			console.log(`Yes! TypeScript typings can be found in the "@types/${package}" package.`);
 			break;
 		default:
 			throw new Error(`Unknown result: ${value}`);
 	}
+
+	console.log('');
 }
 
